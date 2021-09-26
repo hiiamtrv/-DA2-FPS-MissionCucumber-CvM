b577598c31be35a107994b684852abef6f349840
Vo Thanh Trung <66295737+UIT-SE-TrungVo@users.noreply.github.com>
2021-09-26 17:11:51 +0700

    feat: init Event Center (not tested)

---
+++ Assets/Src/Bases/EventCenter/EventCenter.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class EventCenter
+{
+    public static void Subcribe(string eventId, Action<object> action)
+    {
+        GetInstance().AddSubriber(eventId, action);
+    }
+
+    public static void Unsubcribe(string eventId, Action<object> action)
+    {
+        GetInstance().RemoveSubcriber(eventId, action);
+    }
+
+    public static void Publish(string eventId, object data)
+    {
+        GetInstance().Notify(eventId, data);
+    }
+
+    public static void Publish(string eventId)
+    {
+        GetInstance().Notify(eventId, null);
+    }
+}
+
+#region PRIVATE METHODS
+public partial class EventCenter
+{
+    static EventCenter _instance = null;
+    private static EventCenter GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new EventCenter();
+        }
+        return _instance;
+    }
+
+    Dictionary<String, List<Action<object>>> _eventMap;
+
+    EventCenter()
+    {
+        _eventMap = new Dictionary<String, List<Action<object>>>();
+    }
+
+    List<Action<object>> GetSubribers(string eventId)
+    {
+        if (_eventMap.ContainsKey(eventId))
+        {
+            _eventMap.Add(
+                eventId,
+                new List<Action<object>>()
+            );
+        }
+        return _eventMap[eventId];
+    }
+
+    void AddSubriber(string eventId, Action<object> action)
+    {
+        List<Action<object>> subcribers = this.GetSubribers(eventId);
+        subcribers.Add(action);
+    }
+
+    void RemoveSubcriber(string eventId, Action<object> action)
+    {
+        List<Action<object>> subcribers = this.GetSubribers(eventId);
+        subcribers.Remove(action);
+    }
+
+    void Notify(string eventId, object data)
+    {
+        var subcribers = GetInstance().GetSubribers(eventId);
+        foreach (Action<object> action in subcribers)
+        {
+            action(data);
+        }
+    }
+}
+#endregion
---
+++ Assets/Src/Bases/EventCenter/EventIds.cs
@@ -0,0 +1,4 @@
+static partial class EventId {
+    public const string MOUSE_DOWN = "MOUSE_DOWN";
+    public const string MOUSE_MOVE = "MOUSE_MOVE";
+}
