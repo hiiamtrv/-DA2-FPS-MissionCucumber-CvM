1046be517f932932028aceae549e71c4b58a7b03
Vo Thanh Trung <66295737+UIT-SE-TrungVo@users.noreply.github.com>
2021-10-01 19:48:28 +0700

    no message

--- Assets/Src/Const/KeyBinds.cs
+++ Assets/Src/Const/KeyBinds.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 
-class KeyBind {
+class KeyBind
+{
     public KeyCode SKILL_1 = KeyCode.Q;
     public KeyCode SKILL_2 = KeyCode.E;
     public KeyCode INTERACT = KeyCode.F;
-    public KeyCode DUCK = KeyCode.LeftControl;
+    public KeyCode CROUCH = KeyCode.LeftControl;
     public KeyCode SPRINT = KeyCode.LeftShift;
     public KeyCode JUMP = KeyCode.Space;
 }
