b8e66df83eb745414b8a0285d6ac77ae1fda081a
Vo Thanh Trung (SeuitTrV) <trungvo482@gmail.com>
2021-12-18 11:34:45 +0700

    #restore cat number

--- Assets/Src/Modules/Game/Network/NetworkGame.cs
+++ Assets/Src/Modules/Game/Network/NetworkGame.cs
@@ -11,7 +11,7 @@ using BayatGames.Serialization.Formatters.Json;
 
 public class NetworkGame : BaseNetwork
 {
-    public const int NUM_CATS_SLOT = 1;
+    public const int NUM_CATS_SLOT = 2;
     public const int NUM_MICE_SLOT = 4;
 
     public const int NUM_CUCUMBER_POINT = 10;
