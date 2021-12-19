155f685a9f57d62f102cc339c134a5a84d556040
Vo Thanh Trung (SeuitTrV) <trungvo482@gmail.com>
2021-12-19 17:02:15 +0700

    #HUD: num cat & mouse

--- Assets/Src/Modules/Game/Base/Hud/ObjectiveProcess/CanvasObjectiveProcess.cs
+++ Assets/Src/Modules/Game/Base/Hud/ObjectiveProcess/CanvasObjectiveProcess.cs
@@ -15,21 +15,32 @@ namespace GameHud
         Text _lbLastCucumber;
         Slider _slider;
 
+        Text _lbNumAlly;
+        Text _lbNumEnemy;
+        Text _lbAlly;
+        Text _lbEnemy;
+
         GameObject _player;
         int _cucumberObtained;
 
+        CharacterSide ally;
+        CharacterSide enemy;
+
+        int _numAlly;
+        int _numEnemy;
+
         void Awake()
         {
-            EventCenter.Subcribe(EventId.CREATE_PLAYER, (data) =>
-            {
-                this._player = GameVar.Ins.Player;
-                CharacterSide side = this._player.GetComponent<CharacterStats>().CharacterSide;
-                Debug.Log("Get character side", side);
+            // EventCenter.Subcribe(EventId.CREATE_PLAYER, (data) =>
+            // {
+            //     this._player = GameVar.Ins.Player;
+            //     CharacterSide side = this._player.GetComponent<CharacterStats>().CharacterSide;
+            //     Debug.Log("Get character side", side);
 
-                this._sliderCat.gameObject.SetActive(side == CharacterSide.CATS);
-                this._sliderMouse.gameObject.SetActive(side == CharacterSide.MICE);
-                this._cucumberObtained = 0;
-            });
+            //     this._sliderCat.gameObject.SetActive(side == CharacterSide.CATS);
+            //     this._sliderMouse.gameObject.SetActive(side == CharacterSide.MICE);
+            //     this._cucumberObtained = 0;
+            // });
 
             EventCenter.Subcribe(EventId.CUCUMBER_OBTAINED, (pubData) =>
             {
@@ -41,6 +52,15 @@ namespace GameHud
                     this._lbLastCucumber.gameObject.SetActive(true);
                 }
             });
+
+            EventCenter.Subcribe(EventId.CHARACTER_ELIMINATED, (e) =>
+            {
+                GameObject dieObject = e as GameObject;
+                CharacterSide side = dieObject.GetComponent<CharacterStats>().CharacterSide;
+                if (side == GameVar.StartSide) _numAlly--;
+                if (side != GameVar.StartSide) _numEnemy--;
+                this.UpdateCharNumber();
+            });
         }
 
         void Start()
@@ -50,6 +70,11 @@ namespace GameHud
             this._sliderMouse = uiHelper.ui[SLIDER_MOUSE].GetComponent<Slider>();
             this._lbLastCucumber = uiHelper.ui[LB_LAST_CUCUMBER].GetComponent<Text>();
 
+            this._lbAlly = uiHelper.ui[LB_ALLY].GetComponent<Text>();
+            this._lbEnemy = uiHelper.ui[LB_ENEMY].GetComponent<Text>();
+            this._lbNumAlly = uiHelper.ui[LB_NUM_ALLY].GetComponent<Text>();
+            this._lbNumEnemy = uiHelper.ui[LB_NUM_ENEMY].GetComponent<Text>();
+            
             this._lbLastCucumber.gameObject.SetActive(false);
 
             this._sliderCat.value = 0;
@@ -60,17 +85,45 @@ namespace GameHud
         {
             if (this._player == null && GameVar.Ins.Player != null)
             {
-                this._player = GameVar.Ins.Player; 
+                this._player = GameVar.Ins.Player;
                 CharacterSide side = this._player.GetComponent<CharacterStats>().CharacterSide;
 
                 this._sliderCat.gameObject.SetActive(side == CharacterSide.CATS);
                 this._sliderMouse.gameObject.SetActive(side == CharacterSide.MICE);
                 this._cucumberObtained = 0;
+
+                switch (side)
+                {
+                    case CharacterSide.CATS:
+                        this._numAlly = NetworkGame.NUM_CATS_SLOT;
+                        this._numEnemy = NetworkGame.NUM_MICE_SLOT;
+                        this._lbAlly.text = "Cats";
+                        this._lbEnemy.text = "Mouse";
+                        this.UpdateCharNumber();
+                        break;
+                    case CharacterSide.MICE:
+                        this._numEnemy = NetworkGame.NUM_CATS_SLOT;
+                        this._numAlly = NetworkGame.NUM_MICE_SLOT;
+                        this._lbEnemy.text = "Cats";
+                        this._lbAlly.text = "Mouse";
+                        this.UpdateCharNumber();
+                        break;
+                }
             }
+        }
+
+        void UpdateCharNumber()
+        {
+            this._lbNumAlly.text = _numAlly.ToString();
+            this._lbNumEnemy.text = _numEnemy.ToString();
         }
 
         const string SLIDER_MOUSE = "SliderMouse";
         const string SLIDER_CAT = "SliderCat";
         const string LB_LAST_CUCUMBER = "LbLastCucumber";
+        const string LB_NUM_ALLY = "LbNumAlly";
+        const string LB_NUM_ENEMY = "LbNumEnemy";
+        const string LB_ALLY = "LbEnemy";
+        const string LB_ENEMY = "LbAlly";
     }
 }
